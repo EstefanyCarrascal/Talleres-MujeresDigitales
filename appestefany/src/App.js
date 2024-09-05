738af4f9911979ba227e3112ce2f201953a7f658
EstefanyCarrascal <estefany_munoz@soy.sena.edu.co>
2024-09-05 00:15:40 -0500

    Proyecto React con taller
    Se creo un proyecto de react dentro de este una carpera assets y se adjunto documento con solución del taller y cap de ejecución del proyecto

---
+++ appestefany/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Saludo from "./Saludo";
+
+function App() {
+  return(
+    <Saludo/>
+  );
+}
+
+export default App;
---
+++ appestefany/src/Saludo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Saludo() {
+    return(
+        <div>
+            <h1>Hola profe</h1>
+        </div>
+    );
+}
+export default Saludo;
